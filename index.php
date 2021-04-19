ce6ec117bb0eafc13c4cdfe5aff5b2a0aa18bef4
Inspire ME <82292429+Inspirame@users.noreply.github.com>
2021-04-18 20:37:59 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta charset="UTF-8">
+  <title>Inspire ME</title>
+  <link rel="stylesheet" href="index.css">
+  <link rel="icon" stylesheet="image/jpg" href="icon.png">
+</head>
+<body>
+  <div id="bmenu" align="center">
+    <img src="bannerimg.png" alt="">
+<div id="menu"><br>
+    <a href="index.html" id="menu">Inicio</a>
+    <a href="tienda.html" id="menu">Tienda</a>
+    <a href="contacto.html" id="menu">Contacto</a></b>
+</div></div>
+  
+  <a href="form1.html"><div class="container">
+    <div class="item">
+    <img src="148412157_267325421682429_1238879508848583207_n.jpg" alt="" class="item-img">
+      <div class="item-text">
+          <h3>Taza</h3>
+        <h4>$ 100</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur.</p>
+      </div>
+    </div></a>
+    
+    <div class="item">
+      <a href=""><img src="159001280_125782126215844_7992214368722143219_n.jpg.jpg" alt="" class="item-img">
+      <div class="item-text">
+        <h3>Alcancia</h3>
+        <h4>$ 100</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur.</p>
+      </div>
+    </div>
+    
+    <div class="item">
+        <a href=""><img src="148412157_267325421682429_1238879508848583207_n.jpg" alt="" class="item-img"></a>
+      <div class="item-text">
+        <h3>Imagen 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur.</p>
+      </div>
+    </div>
+    
+    <div class="item">
+        <a href=""><img src="148412157_267325421682429_1238879508848583207_n.jpg" alt="" class="item-img"></a>
+      <div class="item-text">
+        <h3>Imagen 1</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur.</p>
+      </div>
+    </div>
+  
+</body>
+</html>
